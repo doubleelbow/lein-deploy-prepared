881de8853c4f3dc1cfa4521075999c22150446e7
Luca L. Bow <luca@doubleelbow.com>
2018-11-05 11:28:12 +0100

    Add circleci config

--- project.clj
+++ project.clj
@@ -4,4 +4,10 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :scm {:name "git" :url "https://github.com/doubleelbow/lein-deploy-prepared"}
-  :eval-in-leiningen true)
+  :eval-in-leiningen true
+  :repositories [["snapshots" {:url "https://repo.clojars.org"
+                               :username :env/clojars_user
+                               :password :env/clojars_pass}]
+                 ["releases" {:url "https://repo.clojars.org"
+                              :username :env/clojars_user
+                              :password :env/clojars_pass}]])
